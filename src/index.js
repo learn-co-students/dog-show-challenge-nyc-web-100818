4502ad0499e029d365c4320ebffa8adbe9a84670
magic-onion <alexander.m.truong@gmail.com>
2018-11-30 11:05:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-})
+document.addEventListener('DOMContentLoaded', init)
+function init () {
+  domCon = new DomController
+  fetch('http://localhost:3000/dogs')
+  .then( r => r.json())
+  .then(function (parsedResponse) {
+    parsedResponse.forEach(dog => {
+      let newDog = new Dog(dog)
+    })
+    Dog.renderAll(Dog.all)
+    domCon.appendAllOnPage()
+  })
+}
